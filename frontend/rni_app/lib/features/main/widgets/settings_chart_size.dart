f02c76631d15ed7ce65ef8c0f4508dfc81872b12
Cerulean <Parkorn.T@ku.th>
2026-02-27 18:25:55 +0700

    Feat: Add success state and adjust UI design

--- frontend/rni_app/lib/features/main/widgets/settings_chart_size.dart
+++ frontend/rni_app/lib/features/main/widgets/settings_chart_size.dart
@@ -46,9 +46,10 @@ class _TextFormFieldExampleState extends State<TextFormFieldExample> {
 
   void onChanged(String value) {
     // Nullify forceErrorText if the input changed.
-    if (forceErrorText != null) {
+    if (forceErrorText != null || isSuccess) {
       setState(() {
         forceErrorText = null;
+        isSuccess = false;
       });
     }
   }
@@ -57,35 +58,52 @@ class _TextFormFieldExampleState extends State<TextFormFieldExample> {
     // Providing a default value in case this was called on the
     // first frame, the [fromKey.currentState] will be null.
     final bool isValid = formKey.currentState?.validate() ?? false;
+
     if (!isValid) {
       return;
     }
-
     context.read<ChartProvider>().changeChartTime(
       double.parse(controller.text),
     );
+    await notifySuccess();
   }
 
+  Future<void> notifySuccess() async {
+    // Notify that the operation is completed for 1.5 seconds
+    setState(() {
+      isSuccess = true;
+    });
+    await Future.delayed(const Duration(milliseconds: 1500));
+    setState(() {
+      isSuccess = false;
+    });
+  }
+
+  bool isSuccess = false;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 250,
-      height: 100,
+      width: 320,
+      height: 80,
       child: Form(
         key: formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              forceErrorText: forceErrorText,
-              controller: controller,
-              decoration: const InputDecoration(
-                hintText: 'Chart Time Steps (seconds)',
+        child: Row(
+          children: [
+            SizedBox(
+              width: 250,
+              child: TextFormField(
+                forceErrorText: forceErrorText,
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: 'Chart Time Steps (seconds)',
+                ),
+                validator: validator,
+                onChanged: onChanged,
               ),
-              validator: validator,
-              onChanged: onChanged,
             ),
-            if (isLoading)
-              const CircularProgressIndicator()
+            const SizedBox(width: 8),
+            if (isSuccess)
+              const Icon(Icons.check, color: Colors.green)
             else
               TextButton(onPressed: onSave, child: const Text('Save')),
           ],
